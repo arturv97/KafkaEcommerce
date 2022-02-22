889f7e97712df72b6969f55a7bccabbdb553be73
arturv97 <oliveira.arturv@gmail.com>
2022-02-22 16:28:33 -0300

    third commit

--- src/main/java/org/Artur/ecommerce/EmailService.java
+++ src/main/java/org/Artur/ecommerce/EmailService.java
@@ -1,51 +1,30 @@
 package org.Artur.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class EmailService {
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        // Consome mensagem de um tópico
-        consumer.subscribe(Collections.singleton("ECOMMERCE_SEND_EMAIL"));
-        // Deixa o serviço rodando/escutando all time
-        while (true) {
-            // Verifica se existe mensagem no tópico
-            var records = consumer.poll(Duration.ofMillis(100));
-            // Se a mensagem for encontrada
-            if (!records.isEmpty()) {
-                System.out.println("Encontrei " + records.count() + " registros");
-                for (var record : records) {
-                    System.out.println("------------------------------------------");
-                    System.out.println("Send email");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        // ignoring
-                        e.printStackTrace();
-                    }
-                    System.out.println("Email sent");
-                }
-                continue;
-            }
+        var emailService = new EmailService();
+        try(var service = new KafkaService(EmailService.class.getSimpleName(),
+                "ECOMMERCE_SEND_EMAIL",
+                emailService::parse)) {
+            service.run();
         }
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        // É necessário criar um grupo (FraudDetectorService)
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getName());
-        return properties;
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Send email");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Email sent");
     }
 }
--- src/main/java/org/Artur/ecommerce/NewOrderMain.java
+++ src/main/java/org/Artur/ecommerce/NewOrderMain.java
@@ -1,39 +1,24 @@
 package org.Artur.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-        var value = "123,456,789"; // value  (ID do pedido, usuario e o valor da compra)
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value); // topic, chave e valor
-        // Como send eh assincrono, foi usado get para aguardar a finalizacao
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
+        try (var dispatcher = new KafkaDispatcher()) {
+            // número de partições >= número de consumidores dentro de um grupo
+            // a key é responsável pelo direcionamento das mensagens para as partições - se for constante as mensagens irão para uma partição fixa
+            for (var i = 0; i < 10; i++) {
+                var key = UUID.randomUUID().toString();
+
+                var value = "123,456,789"; // value  (ID do pedido, usuario e o valor da compra)
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+
+                var email = "Thank you for your order! We are processing your order!";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email); // topic, chave e valor
             }
-            System.out.println("Sucesso " + data.topic() + ":::partition " + data.partition() + " / offset " + data.offset() + " / timestamp " + data.timestamp());
-        };
-        var email = "Thank you for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-        producer.send(record, callback).get();
-        producer.send(emailRecord, callback).get();
+        }
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
-    }
 
 }
