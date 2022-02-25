84c0cbee4b52a48434b033cb6a237f86a90e0792
arturv97 <oliveira.arturv@gmail.com>
2022-02-25 08:45:20 -0300

    fourth commit

--- service-fraud-detector/src/main/java/org/Artur/ecommerce/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/org/Artur/ecommerce/FraudDetectorService.java
@@ -2,29 +2,34 @@ package org.Artur.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public class EmailService {
+import java.util.Map;
+
+public class FraudDetectorService {
+
     public static void main(String[] args) {
-        var emailService = new EmailService();
-        try(var service = new KafkaService(EmailService.class.getSimpleName(),
-                "ECOMMERCE_SEND_EMAIL",
-                emailService::parse)) {
+        var fraudService = new FraudDetectorService();
+        try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse,
+                Order.class,
+                Map.of())) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("------------------------------------------");
-        System.out.println("Send email");
+        System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
         System.out.println(record.value());
         System.out.println(record.partition());
         System.out.println(record.offset());
         try {
-            Thread.sleep(1000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             // ignoring
             e.printStackTrace();
         }
-        System.out.println("Email sent");
+        System.out.println("Order processed");
     }
 }
